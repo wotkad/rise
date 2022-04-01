b163746662e6824891962225d73f077f8c140b8b
wotkad <wotkad@gmail.com>
2022-04-01 17:22:15 +0300

    replace scripts connetion

--- src/bundle.js
+++ src/bundle.js
@@ -1,11 +1,5 @@
 import "styles/_app.scss";
 
 $(function () {
-  require("js/preloader/index");
-  require("js/linksChecker/isCurrentPage/index");
-  require("js/linksChecker/checkInternalLinks/index");
-  require("js/routing/index");
-  require("js/scroll/smoothScroll/index");
-  require("js/scroll/scrollReveal/index");
   require("js/app");
 });
