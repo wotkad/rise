9638ad045367df54cd71f00cb69b173ff8fa3293
wotkad <wotkad@gmail.com>
2022-04-04 14:55:55 +0300

    remove sitemap and update content

--- sitemap.js
+++
@@ -1,8 +0,0 @@
-const SitemapGenerator = require('sitemap-generator');
-const generator = SitemapGenerator('http://localhost:8080', {
-  stripQuerystring: false,
-  filepath: './dist/sitemap.xml',
-  lastMod: new Date().toISOString(),
-  changeFreq: 'monthly',
-});
-generator.start();
