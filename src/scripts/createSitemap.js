91e9c4bc16eb462e750bda791e44d0c687a98a2b
wotkad <wotkad@gmail.com>
2022-04-10 12:15:12 +0300

    update sitemap generator

--- src/scripts/createSitemap.js
+++
@@ -1,8 +0,0 @@
-const SitemapGenerator = require('sitemap-generator');
-const generator = SitemapGenerator('http://localhost:8080', {
-	stripQuerystring: false,
-	filepath: './src/sitemap.xml',
-	lastMod: new Date().toISOString(),
-	changeFreq: 'monthly',
-});
-generator.start();
