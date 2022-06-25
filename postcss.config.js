c2a1bcbaf3a9ce4f97c3699852591ac72f282936
wotkad <wotkad@gmail.com>
2022-06-25 20:44:18 +0300

    fix autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss': {},
-    'autoprefixer': {},
-  },
+  plugins: [
+    require('postcss-import')({}),
+    require('tailwindcss')({}),
+    autoprefixer({
+      overrideBrowserslist: ['> 1%', 'last 4 versions'],
+    }),
+  ],
 };
