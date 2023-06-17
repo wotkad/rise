1d004a20e8382267f21a21d42766113a67752131
wotkad <wotkad@gmail.com>
2023-06-18 01:52:15 +0300

    fix

--- node_scripts/archive-paths.js
+++ node_scripts/archive-paths.js
@@ -1,20 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-// Define the base directory
-const baseDir = 'build'; // Assuming "build" is the parent folder
+const baseDir = 'build';
 
-// Define the levels and their corresponding relative paths
 const levels = {
   1: '.',
   2: '..',
   3: '../..',
 };
 
-// Define the target directory
 const targetDir = '/assets';
 
-// Recursive function to process files in the directory structure
 function processFilesInDirectory(dir, level) {
   const files = fs.readdirSync(dir);
   files.forEach((file) => {
@@ -31,5 +27,4 @@ function processFilesInDirectory(dir, level) {
   });
 }
 
-// Start processing files from the base directory
 processFilesInDirectory(baseDir, 1);
