a9ca42875b40c3c0f03c9e4c9797029ca29114f6
wotkad <wotkad@gmail.com>
2023-08-27 17:14:34 +0300

    update

--- node_scripts/svg-clear.js
+++ node_scripts/svg-clear.js
@@ -1,41 +1,54 @@
 const fs = require('fs');
 const path = require('path');
 const html2pug = require('html2pug');
+
 function removeCommasWithinAttributes(input) {
     const regex = /([a-zA-Z-]+)="([^"]+)",?\s*/g;
     return input.replace(regex, '$1="$2" ');
 }
+
 function processPugFile(filePath) {
     const fileContents = fs.readFileSync(filePath, 'utf8');
     const lines = fileContents.split('\n');
     const outputLines = [];
     let indent = '';
+    let svgLines = [];
+    let insideSvgTag = false;
     let converted = false;
+
     for (const line of lines) {
         if (line.includes('<svg')) {
             indent = line.match(/^\s*/)[0];
-            const modifiedSvgLine = line.replace(/,(\s*[^=]+="[^"]*")/g, '$1');
-            const pugCode = html2pug(modifiedSvgLine.trim(), { fragment: true, doubleQuotes: true });
-            const cleanedPugCode = removeCommasWithinAttributes(pugCode)
-                .replace(/\( /g, '(')
-                .replace(/ \)/g, ')');
-            const pugLines = cleanedPugCode.split('\n').map(pugLine => indent + pugLine).join('\n');
-            outputLines.push(pugLines);
-            if (pugCode !== cleanedPugCode) {
-                converted = true;
+            insideSvgTag = true;
+            svgLines.push(line.trim());
+        } else if (insideSvgTag) {
+            svgLines.push(line.trim());
+            if (line.includes('</svg>')) {
+                insideSvgTag = false;
+                const modifiedSvgCode = svgLines.join(' ');
+                const pugCode = html2pug(modifiedSvgCode, { fragment: true, doubleQuotes: true });
+                const cleanedPugCode = removeCommasWithinAttributes(pugCode)
+                    .replace(/\( /g, '(')
+                    .replace(/ \)/g, ')');
+                const pugLines = cleanedPugCode.split('\n').map(pugLine => indent + pugLine).join('\n');
+                outputLines.push(pugLines);
+                svgLines = [];
+                if (pugCode !== cleanedPugCode) {
+                    converted = true;
+                }
             }
         } else {
             outputLines.push(line);
         }
     }
+
     if (converted) {
         const newContent = outputLines.join('\n');
         fs.writeFileSync(filePath, newContent, 'utf8');
         console.log(`✅ Converted inline <svg> tags into PUG in: ${filePath}`);
     }
-    const newContent = outputLines.join('\n');
-    fs.writeFileSync(filePath, newContent, 'utf8');
 }
+
 function processDirectory(directoryPath) {
     const files = fs.readdirSync(directoryPath);
     files.forEach(file => {
@@ -47,5 +60,6 @@ function processDirectory(directoryPath) {
         }
     });
 }
+
 const targetDirectory = path.join(__dirname, '../src/views');
 processDirectory(targetDirectory);
