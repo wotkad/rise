3fea2e15659913aff00f4d2e05a5f182e6ad1b2a
wotkad <wotkad@gmail.com>
2025-04-10 11:36:16 +0300

    fix

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -16,6 +16,5 @@ import "./base/common/scroll-reveal";
 
 import "./base/checks/check-internal-links";
 import "./base/checks/check-target";
-import "./base/checks/check-images";
 import "./base/common/get-current-year";
 import "./base/common/get-header-height";
