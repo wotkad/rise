d3f8dd88a240dbe89e1edafb789ef39bcd7f5dd2
wotkad <wotkad@gmail.com>
2025-05-03 15:39:43 +0300

    update

--- src/assets/js/base/routing/routing-functions.js
+++ src/assets/js/base/routing/routing-functions.js
@@ -1,7 +1,11 @@
 import checkCurrentPage from "../checks/check-current-page";
 import currentYear from "../common/get-current-year";
+import setNbsp from "../common/set-nbsp";
+import setSvgSize from "../common/set-svg-size";
 
 export default function routingFunctions() {
   checkCurrentPage();
   currentYear();
+  setNbsp();
+  setSvgSize();
 }
