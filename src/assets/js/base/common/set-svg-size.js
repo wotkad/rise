d1270aadf9bd154eafd820ca4042cb8278fe50ef
wotkad <wotkad@gmail.com>
2025-10-22 03:06:00 +0300

    update

--- src/assets/js/base/common/set-svg-size.js
+++ src/assets/js/base/common/set-svg-size.js
@@ -2,22 +2,20 @@ export default function setSvgSize() {
   $('svg > use').each(function () {
     const $useElement = $(this);
     const href = $useElement.attr('xlink:href') || $useElement.attr('href');
-    
-    if (href) {
-      const id = href.replace('#', '');
-      const $symbol = $(`#${id}`);
-      
-      if ($symbol.length) {
-        const $svg = $useElement.closest('svg');
-        const viewBox = $symbol.attr('viewBox');
-        const width = $symbol.attr('width');
-        const height = $symbol.attr('height');
-        
-        if (viewBox) $svg.attr('viewBox', viewBox);
-        if (width) $svg.attr('width', width);
-        if (height) $svg.attr('height', height);
-      }
-    }
+
+    if (!href) return;
+
+    const $symbol = $(href);
+    if (!$symbol.length) return;
+
+    const $svg = $useElement.closest('svg');
+    const viewBox = $symbol.attr('viewBox');
+    const width = $symbol.attr('width');
+    const height = $symbol.attr('height');
+
+    if (viewBox) $svg.attr('viewBox', viewBox);
+    if (width) $svg.attr('width', width);
+    if (height) $svg.attr('height', height);
   });
 }
 setSvgSize();
