a22c36dd7b2c0c71ae9a205fdac60440353de6d7
wotkad <wotkad@gmail.com>
2025-10-25 02:57:33 +0300

    update

--- node_scripts/rise/update.js
+++ node_scripts/rise/update.js
@@ -1,10 +1,14 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
 const projectDir = process.cwd();
 const excludeDirName = 'src';
+
 const tempDir = path.join(projectDir, 'rise-temp');
+
 const repoUrl = 'https://github.com/wotkad/rise.git';
 
 function copyFiles(src, dest) {
@@ -25,12 +29,14 @@ function copyFiles(src, dest) {
 function updateTemplate() {
   try {
     execSync(`git clone ${repoUrl} ${tempDir}`, { stdio: 'inherit' });
+
     copyFiles(tempDir, projectDir);
+
     fs.rmSync(tempDir, { recursive: true, force: true });
+
     console.log('Шаблон успешно обновлён!');
   } catch (err) {
     console.error('Ошибка при обновлении шаблона:', err.message);
   }
 }
-
 updateTemplate();
