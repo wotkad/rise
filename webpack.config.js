ff5c5dd9c95c43503875424babd5c3499402fa42
wotkad <wotkad@gmail.com>
2025-11-18 11:32:19 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -218,40 +218,6 @@ module.exports = (env) => {
           parallel: true,
         }),
         new CssMinimizerPlugin(),
-        new ImageMinimizerPlugin({
-          minimizer: {
-            implementation: ImageMinimizerPlugin.imageminGenerate,
-            options: {
-              plugins: [
-                ["gifsicle", { interlaced: true }],
-                ["imagemin-mozjpeg", { progressive: true }],
-                ["imagemin-pngquant", { optimizationLevel: 5 }],
-                [
-                  "svgo",
-                  {
-                    plugins: [
-                      {
-                        name: "preset-default",
-                        params: {
-                          overrides: {
-                            removeViewBox: false,
-                            addAttributesToSVGElement: {
-                              params: {
-                                attributes: [
-                                  { xmlns: "http://www.w3.org/2000/svg" },
-                                ],
-                              },
-                            },
-                          },
-                        },
-                      },
-                    ],
-                  },
-                ],
-              ],
-            },
-          },
-        }),
       ],
       splitChunks: {
         cacheGroups: {
