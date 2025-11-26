2782d2a1237c21ac4565775c82516a1b9030070b
wotkad <wotkad@gmail.com>
2025-11-26 18:48:22 +0300

    update

--- plugins/constructor/components/features/v1/feature/_defaults.js
+++ plugins/constructor/components/features/v1/feature/_defaults.js
@@ -1,14 +1,14 @@
 module.exports = {
   defaults: {
-    title: 'Заголовок',
     description: 'Описание',
     icon: 'Перейти',
+    title: 'Заголовок',
   },
   mergeConfig(data, defaults) {
     return {
-      title: data.title !== undefined ? data.title : defaults.title,
       description: data.description !== undefined ? data.description : defaults.description,
       icon: data.icon !== undefined ? data.icon : defaults.icon,
+      title: data.title !== undefined ? data.title : defaults.title,
     }
   },
 };
