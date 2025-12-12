f36d8cec0557ac0cf39b0d017b39486c455bcedd
wotkad <wotkad@gmail.com>
2025-12-13 00:11:13 +0300

    update

--- src/bundle.js
+++ src/bundle.js
@@ -1,5 +1,12 @@
-import "@styles/app.scss";
+if ($('.swiper')) {
+  import('swiper/css');
+  import('swiper/css/pagination');
+  import('swiper/css/grid');
+}
+
 import "highlight.js/scss/atom-one-dark.scss";
+
+import "@styles/app.scss";
 
 if (module.hot) {
   module.hot.accept();
