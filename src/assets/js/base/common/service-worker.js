7bc4989a7f9908f7fb5af76b85275c4ec5c1b745
wotkad <wotkad@gmail.com>
2025-12-14 00:25:26 +0300

    update

--- src/assets/js/base/common/service-worker.js
+++ src/assets/js/base/common/service-worker.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/assets/js/base/common/service-worker.js')
-      .then(reg => console.log('[SW] Registered', reg))
+      .then(reg => console.log('[SW] Registered'))
       .catch(err => console.error('[SW] Registration failed', err));
   });
 }
