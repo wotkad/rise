697f8b86ef2b5b8473aec5b4f7c70977387dd59f
wotkad <wotkad@gmail.com>
2025-12-25 20:23:17 +0300

    update

--- src/assets/js/base/common/placeholder-images.js
+++ src/assets/js/base/common/placeholder-images.js
@@ -1,4 +1,4 @@
-export default function placeholderImages(placeholderSrc = 'https://placehold.co/600x400') {
+export default function placeholderImages(placeholderSrc = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgdmlld0JveD0iMCAwIDEwMCAxMDAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxyZWN0IHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIiBmaWxsPSIjOUNBM0FGIi8+Cjwvc3ZnPgo=') {
   $('img').each(function () {
     const $img = $(this);
 
